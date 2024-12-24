3e18c6031e0d3705b3f4876f4a25bf2b1e938414
hinase01 <hikaruhinase@yahoo.com>
2024-12-24 19:58:56 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -73,3 +73,4 @@ Temporary commit content for 2024-10-07T19:58:56
 Temporary commit content for 2024-10-26T19:58:56
 Temporary commit content for 2024-11-04T19:58:56
 Temporary commit content for 2024-11-11T19:58:56
+Temporary commit content for 2024-12-24T19:58:56
